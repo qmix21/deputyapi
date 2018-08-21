8c697937d2109cf529e220030579a6fad113e789
qmix21 <jamesredirect@vps.ausgpl.com>
2018-08-21 23:30:44 +1000

    update

---
+++ app/Http/Controllers/Deputy.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DeputyController;
+use Illuminate\Http\Request;
+
+class Deputy extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DeputyController  $deputyController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DeputyController $deputyController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DeputyController  $deputyController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DeputyController $deputyController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DeputyController  $deputyController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DeputyController $deputyController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DeputyController  $deputyController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DeputyController $deputyController)
+    {
+        //
+    }
+}
