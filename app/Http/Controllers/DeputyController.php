fc50c3ff514acad1c565e948e152080e7626e9c5
James Mahon <james@ausgpl.com>
2018-08-22 02:08:08 +1000

    test

--- app/Http/Controllers/DeputyController.php
+++ app/Http/Controllers/DeputyController.php
@@ -24,7 +24,7 @@ class DeputyController extends Controller
             'Authorization' => 'Bearer '. $accessToken,
             'Accept' => 'application/json',
         ];
-        $res = $client->request('GET','my/Timesheet',['headers' => $headers]);
+        $res = $client->request('GET','my/timesheet',['headers' => $headers]);
         $res->getStatusCode(); // 200
         $res->getBody(); // { "type": "User", ....
         return $res;
