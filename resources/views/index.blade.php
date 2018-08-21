d66229a2f6c7bfd58cd4a772772dc05379a45cd6
James Mahon <james@ausgpl.com>
2018-08-22 01:49:38 +1000

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <html>
 
 
-<form action="/post" method="post">
+<form action="/deputy/public/post" method="post">
 <input type="text" name="timesheet">
 <input type="text" name="mealbreak">
 <button type="submit">
