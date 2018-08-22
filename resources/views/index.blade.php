cc78a7f10cf15d410b27f8296095c915670787d3
James Mahon <james@ausgpl.com>
2018-08-22 14:56:18 +1000

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,16 @@
 	{{csrf_field()}}
 <input type="text" name="timesheet">
 <input type="text" name="mealbreak">
-<button type="submit">
+<button type="submit"></button>
 </form>
 
+<div>
 <form action="/deputy/public/start" method="post">
 
 	{{csrf_field()}}
 <input type="text" name="empId">
 <input type="text" name="unitId">
-<button type="submit">
+<button type="submit"></button>
+</div>
 </form>
 </html>
